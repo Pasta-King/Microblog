1b4dfaff7e301b087a318ebf400a81da2e846825
Pasta-King <caiopasta@gmail.com>
2019-10-06 20:54:38 +0100

    06/10/2019

--- app/__init__.py
+++ app/__init__.py
@@ -3,6 +3,9 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+import logging
+from logging.handlers import SMTPHandler, RotatingFileHandler
+import os
 
 myApp = Flask(__name__)
 myApp.config.from_object(Config)
@@ -11,5 +14,32 @@ db = SQLAlchemy(myApp)
 migrate = Migrate(myApp, db)
 login = LoginManager(myApp)
 login.login_view = 'login'
+
+if not myApp.debug: # Couldn't get the email to send
+    if myApp.config["MAIL_SERVER"]: 
+        auth = None
+        if myApp.config["MAIL_USERNAME"] or myApp.config["MAIL_PASSWORD"]:
+            auth = (myApp.config["MAIL_USERNAME"], myApp.config["MAIL_PASSWORD"])
+        secure = None
+        if myApp.config['MAIL_USE_TLS']:
+            secure = ()
+        mail_handler = SMTPHandler(
+            mailhost=(myApp.config["MAIL_SERVER"], myApp.config["MAIL_PORT"]),
+            fromaddr="caiopasta@" + myApp.config["MAIL_SERVER"],
+            toaddrs=myApp.config["ADMINS"], subject="Microblog Failure",
+            credentials=auth, secure=secure)
+        mail_handler.setLevel(logging.ERROR)
+        myApp.logger.addHandler(mail_handler)
+    
+    if not os.path.exists("logs"):
+        os.mkdir("logs")
+    file_handler = RotatingFileHandler("logs/microblog.log", maxBytes=10240, backupCount=10)
+    file_handler.setFormatter(logging.Formatter(
+        "%(asctime)s %(levelname)s: %(message)s [ in %(pathname)s:%(lineno)d]"
+    ))
+    file_handler.setLevel(logging.INFO)
+    myApp.logger.addHandler(file_handler)
+    myApp.logger.setLevel(logging.INFO)
+    myApp.logger.info("Microblog startup")
 
 from app import routes, models, errors
