4768d6ba1694803fdd99d0a30ef616e4b7c75e01
Pasta-King <caiopasta@gmail.com>
2019-10-28 12:47:00 +0000

    27/10/19 - Pagination

--- config.py
+++ config.py
@@ -12,3 +12,4 @@ class Config(object):
     MAIL_USERNAME = os.environ.get("MAIL_USERNAME")
     MAIL_PASSWORD = os.environ.get("MAIL_PASSWORD")
     ADMINS = ["caiopasta@gmail.com"]
+    POSTS_PER_PAGE = 3 # Outside of testing use >3 posts
